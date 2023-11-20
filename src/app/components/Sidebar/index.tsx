b70de3fc5594fc797b73cce046b2537cf8405c84
Guilherme Girardi <guilherme@ardanis.com>
2023-11-20 16:03:48 -0300

    create MainNavigation component

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import { Search } from 'lucide-react'
+import { MainNavigation } from './MainNavigation'
 import { Logo } from '../icons/Logo'
 
 export function Sidebar() {
@@ -6,13 +7,15 @@ export function Sidebar() {
     <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-200 px-3 py-2 shadow-sm">
+      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-200 px-3 py-2 shadow-sm">
         <Search className="h-5 w-5 text-zinc-500" />
         <input
           className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
           placeholder="Search"
         />
       </div>
+
+      <MainNavigation />
     </aside>
   )
 }
