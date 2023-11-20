f4402bf14556614b6ca2dd33ebce763e710daab0
Guilherme Girardi <guilherme@ardanis.com>
2023-11-20 15:39:51 -0300

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
