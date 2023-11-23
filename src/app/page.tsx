9608233220ab4c36ddfa300882361a00266f5bdb
Guilherme Girardi <guilherme@ardanis.com>
2023-11-23 19:59:12 -0300

    create sections for main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Mail } from 'lucide-react'
 import { SettingsTabs } from './components/SettingsTabs'
+import * as Input from './components/Sidebar/Input'
 
 export default function Home() {
   return (
@@ -32,8 +34,129 @@ export default function Home() {
           </div>
         </div>
 
-        <form id="settings" action="" className="mt-6 flex w-full flex-col">
-          form
+        <form
+          id="settings"
+          action=""
+          className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200"
+        >
+          <div className="grid-cols-form grid gap-3">
+            <label
+              htmlFor="firstName"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Name
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <Input.Root>
+                <Input.Control id="firstName" defaultValue="Guilherme" />
+              </Input.Root>
+
+              <Input.Root>
+                <Input.Control defaultValue="Girardi" />
+              </Input.Root>
+            </div>
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label
+              htmlFor="firstName"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Email address
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <Input.Root>
+                <Input.Prefix>
+                  <Mail className="h-5 w-5 text-zinc-500" />
+                </Input.Prefix>
+
+                <Input.Control
+                  id="email"
+                  type="email"
+                  defaultValue="gui@gui.com"
+                />
+              </Input.Root>
+            </div>
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label
+              htmlFor="photo"
+              className="text-sm font-medium text-zinc-700 "
+            >
+              Your photo
+              <span className="text-md mt-0.5 block font-normal text-zinc-500">
+                This will be displayed on your profile.
+              </span>
+            </label>
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label htmlFor="role" className="text-sm font-medium text-zinc-700">
+              Role
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <Input.Root>
+                <Input.Control id="role" type="text" defaultValue="CTO" />
+              </Input.Root>
+            </div>
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label
+              htmlFor="country"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Country
+            </label>
+            <div className="grid grid-cols-2 gap-6"></div>
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label
+              htmlFor="timezone"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Timezone
+            </label>
+            <div className="grid grid-cols-2 gap-6"></div>
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label htmlFor="bio" className="text-sm font-medium text-zinc-700 ">
+              Bio
+              <span className="text-md mt-0.5 block font-normal text-zinc-500">
+                Write a short introduction.
+              </span>
+            </label>
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label
+              htmlFor="portfolio"
+              className="text-sm font-medium text-zinc-700 "
+            >
+              Portfolio projects
+              <span className="text-md mt-0.5 block font-normal text-zinc-500">
+                Share a few snippets of yours work.
+              </span>
+            </label>
+          </div>
+
+          <div className="flex items-center justify-end gap-2 pt-5">
+            <button
+              type="button"
+              className="rounded-lg border border-zinc-900 px-4  py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              name="settings"
+              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
         </form>
       </div>
     </>
